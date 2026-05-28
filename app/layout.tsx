ead3b044bc3bd9cbe6d885b52953fa6f25343469
Mohamed Ashraf <mohamedashraff200@gmail.com>
2026-05-28 16:24:42 +0300

    sdfg

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,7 +34,7 @@ export default function RootLayout({
           <div className="font-bold text-3xl">
     <h1>Profile</h1>
   </div>
-          <div className="flex   gap-6    items-center   ">
+          <div className="flex   gap-6  flex-wrap justify-center overflow-x-hidden  items-center   ">
     <Link href="/" className="px-3 py-2 hover:bg-gray-200 rounded-lg transition-colors duration-300 ">Home</Link>
     <Link href="/About" className="px-3 py-2 hover:bg-gray-200 rounded-lg transition-colors duration-300">About</Link>
     <Link href="/project" className="px-3 py-2 hover:bg-gray-200 rounded-lg transition-colors duration-300">Projects</Link>
