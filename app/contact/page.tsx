0009a0ad46ec342d9c1b20cd654abd95cf3f6ebf
Mohamed Ashraf <mohamedashraff200@gmail.com>
2026-05-29 14:26:21 +0300

    save

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,7 +1,50 @@
-
+import Link from "next/link";
+import { FaBahai, FaGithub,FaLinkedin,FaEarlybirds,FaSave } from "react-icons/fa";
+import { SiNextdotjs,SiMealie } from "react-icons/si";
 
 export default function Contact(){
     return(
-        <h1>Contact</h1>
+        <div  className="min-h-screen flex  bg-gray-500 flex-col items-center justify-center p-8">
+            <h1 className="text-3xl font-bold mb-6">Contact me</h1>
+
+            {/* نص تعريفي */}
+            <p  className="text-lg text-white mb-8 text-center max-w-xl">If you would like to contact me regarding projects or job
+                 opportunities, you can send me a message
+                  or contact me through these links.</p>
+
+                  {/* روابط التواصل */}
+                  <div  className="flex gap-6">
+                    <Link  href="mohamedashraff200@gmail.com"  className="px-4 py-2 bg-gray-800 text-white flex  gap-1    items-center rounded-2xl hover:bg-gray-700"> <SiMealie/> Email<SiMealie/></Link>
+                     <Link  href="https://www.linkedin.com/in/mohamed-ashraf-7270b223b"  className="px-4 py-2 bg-gray-800 flex  gap-1    items-center text-white rounded-2xl hover:bg-gray-700"> <FaLinkedin/>LinkedIn <FaLinkedin/></Link>
+                     <Link  href="https://github.com/Mohamed-Ashraf55"  className="px-4 py-2 bg-gray-800 text-white rounded-2xl flex  gap-1    items-center hover:bg-gray-700"><FaGithub/> GitHub  <FaGithub/></Link>
+
+
+                  </div>
+
+                  {/* فورم للتواصل */}
+                  <form className="mt-10 w-full max-w-md bg-gray-700 shadow-md rounded-3xl p-6 space-y-4">
+        <input
+          type="text"
+          placeholder="اسمك"
+          className="w-full border border-gray-300 rounded-2xl px-3 py-2 focus:outline-none focus:ring focus:ring-blue-200"
+        />
+        <input
+          type="email"
+          placeholder="بريدك الإلكتروني"
+          className="w-full border border-gray-300 rounded-2xl px-3 py-2 focus:outline-none focus:ring focus:ring-blue-200"
+        />
+        <textarea
+          placeholder="رسالتك"
+          className="w-full border border-gray-300 rounded-2xl px-3 py-2 focus:outline-none focus:ring focus:ring-blue-200"
+        ></textarea>
+        <button
+          type="submit"
+          className="w-full bg-gray-900 items-center justify-center  flex p-1 gap-2 text-white py-2 rounded-2xl hover:bg-gray-600"
+        >
+           submit <FaSave  />
+        </button>
+      </form>
+
+        </div>
     )
 }
